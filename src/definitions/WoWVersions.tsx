db1bf2b8e8bad438d0f80439dee0110ac7d7bdc5
wutname1 <jeremy12@gmail.com>
2020-10-24 00:08:46 -0500

    Cleanup of folder scan code

---
+++ src/definitions/WoWVersions.tsx
@@ -0,0 +1,6 @@
+export const WoWVersionRefrence = [
+  { folder: "_beta_", name: "Beta" },
+  { folder: "_classic_", name: "Classic" },
+  { folder: "_retail_", name: "Shadowlands" },
+  { folder: "_classic_ptr_", name: "Classic PTR" },
+];
--- src/services/native/win/fileReadWrite.ts
+++ src/services/native/win/fileReadWrite.ts
@@ -1,58 +1,45 @@
+import { WoWVersionRefrence } from "../../../definitions/WoWVersions";
+
 const { readdir, rmdir } = window.require("fs").promises;
 const { statSync, existsSync, readdirSync } = window.require("fs");
 const execSync = window.require("child_process").execSync;
-const { join } = window.require("path");
 
-export const getAllDirs = async (path:string) => {
+export const getAllDirs = async (path: string) => {
   let dirs: any[] = [];
   for (const file of await readdir(path)) {
     let cleanName = file.replace(/_/g, "");
     cleanName = cleanName.charAt(0).toUpperCase() + cleanName.slice(1);
-    const f:string = path + "/" + file;
+
+    const f: string = path + "/" + file;
     const dirInfo = statSync(f);
     //Currently every version of the game has a BlizzardError.exe in the folder so check to see if the folder is a game folder
     //We should run a wildcard check for wow*.exe//
     if (dirInfo.isDirectory() && existsSync(f + "/BlizzardError.exe")) {
-      if (file === "_retail_") {
-        let wowExe = findWowExe(f);
+      let wowExe = findWowExe(f);
+      if (wowExe) {
         let path = f + "/" + wowExe;
-        let version = "";
-        if (existsSync(path)) {
-          let queryPath = path.split("/").join("\\\\");
-          version = getExeVersion(queryPath);
+        let version = getExeVersion(path.split("/").join("\\\\"));
+
+        let wowVer = WoWVersionRefrence.find((e) => {
+          return e.folder === file.toLowerCase();
+        });
+
+        if (wowVer) {
+          dirs.push({ name: wowVer.name, path: f, version });
+        } else {
+          dirs.push({ name: cleanName, path: f });
         }
-        dirs.push({ name: "Retail", path: f, version });
-      } else if (file === "_classic_") {
-        let wowExe = findWowExe(f);
-        let path = f + "/" + wowExe;
-        let version = "";
-        if (existsSync(path)) {
-          let queryPath = path.split("/").join("\\\\");
-          version = getExeVersion(queryPath);
-        }
-        dirs.push({ name: "Classic", path: f, version });
-      } else if (file === "_beta_") {
-        let wowExe = findWowExe(f);
-        let path = f + "/" + wowExe;
-        let version = "";
-        if (existsSync(path)) {
-          let queryPath = path.split("/").join("\\\\");
-          version = getExeVersion(queryPath);
-        }
-        dirs.push({ name: "Beta", path: f, version });
-      } else {
-        dirs.push({ name: cleanName, path: f });
       }
     }
   }
   return dirs;
 };
-const findWowExe = (f:string) => {
+const findWowExe = (f: string) => {
   let files = readdirSync(f);
   for (let i = 0; i < files.length; i++) {
     if (
-      files[i].substring(0, 3) === "wow" &&
-      files[i].substring(files[i].length - 4) === ".exe"
+      files[i].substring(0, 3).toLowerCase() === "wow" &&
+      files[i].substring(files[i].length - 4).toLowerCase() === ".exe"
     ) {
       return files[i];
     }
@@ -60,7 +47,7 @@ const findWowExe = (f:string) => {
   return false;
 };
 
-const getExeVersion = (path:string) => {
+const getExeVersion = (path: string) => {
   let app = `wmic datafile where name="${path}" get version /value`;
   let child = execSync(app).toString();
   if (child) {
@@ -69,7 +56,7 @@ const getExeVersion = (path:string) => {
   return child;
 };
 const deleteFolderRecursive = async (path: string) => {
-  rmdir(path, { recursive: true }, (err:string) => {
+  rmdir(path, { recursive: true }, (err: string) => {
     if (err) {
       console.error(err);
       return false;
