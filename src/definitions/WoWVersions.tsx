5e48a9f2bdcd9838fbb64b03ba3d3be7d7e11504
wutname1 <jeremy12@gmail.com>
2020-10-24 00:10:51 -0500

    last 2 version of wow defined.

--- src/definitions/WoWVersions.tsx
+++ src/definitions/WoWVersions.tsx
@@ -1,6 +1,8 @@
 export const WoWVersionRefrence = [
+  { folder: "_alpha_", name: "Alpha" },
   { folder: "_beta_", name: "Beta" },
   { folder: "_classic_", name: "Classic" },
   { folder: "_retail_", name: "Shadowlands" },
   { folder: "_classic_ptr_", name: "Classic PTR" },
+  { folder: "_ptr_", name: "PTR" },
 ];
