43ad32a307caf647ca18b1194c9537b64cc7294c
Ray <s.ray1100@gmail.com>
2020-10-24 13:07:23 -0700

    Caching: hydrate state upon loading initial setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,8 +2,30 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./reducers/rootReducer";
 import thunk from "redux-thunk";
 
+interface PersistedState {
+  WowDirReducer: any;
+}
+
+//TODO: will need to hydrate our state from a cache
+//hardcoded example
+//changing the wowVerDirList to empty array will enable dir search
+const persistedState: PersistedState = {
+  WowDirReducer: {
+    wowRootDir: "C:/Games/World of Warcraft",
+    wowSelectedDir: "1",
+    //wowVerDirList:[],
+    wowVerDirList: [
+      {
+        name: "Cached",
+        path: "C:/Games/World of Warcraft/_retail_",
+        version: "8.0.0.0",
+      },
+    ],
+  },
+};
+
 const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, persistedState, applyMiddleware(thunk));
 };
 
 export default configureStore;
